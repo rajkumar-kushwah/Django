7c852c72629a98377fa789f93757c34ba276897d
Sonu Lodhi <sp95759201@gmail.com>
2025-01-04 18:10:19 +0530

    add about or contact

--- project/views.py
+++ project/views.py
@@ -2,3 +2,7 @@ from django.http import HttpResponse
 
 def Home(request):
     return HttpResponse("Hello i am Home")
+def about(request):
+    return HttpResponse("Hello i am about")
+def contact(request):
+    return HttpResponse("Hello i am contact")
