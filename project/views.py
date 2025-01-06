1003240e366231f72647e1067a09d5dfa4d0a0ea
Sonu Lodhi <sp95759201@gmail.com>
2025-01-06 17:17:06 +0530

    add template

--- project/views.py
+++ project/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import rendere
 
 def Home(request):
-    return HttpResponse("Hello i am Home")
+    # return HttpResponse("Hello i am Home")
+    return rendere(request, 'index.html')
 def about(request):
     return HttpResponse("Hello i am about")
 def contact(request):
