f41ff66b8bd3309342a9881f61c35bca57f4da72
Sonu Lodhi <sp95759201@gmail.com>
2025-01-08 20:54:51 +0530

    create model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class table(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+  image = models.ImageField(upload_to='media')
