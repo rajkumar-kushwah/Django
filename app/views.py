19f5dd292f4374819e991681d0d03e55168102e4
Sonu Lodhi <sp95759201@gmail.com>
2025-01-12 22:51:39 +0530

    remove all

--- app/views.py
+++ app/views.py
@@ -5,7 +5,6 @@ from .models import posts
 # Create your views here.
 
 def home(req):
-    post = posts.objects.all()
     if req.method == 'POST':
         image = req.POST.get('image')
         title = req.POST.get('title')
@@ -14,4 +13,4 @@ def home(req):
         post = posts(title = title, des= des, image = image)
         post.save()
         return HttpResponse("data added successfully")
-    return render(req, 'app.html', {"posts":post})
+    return render(req, 'app.html', )
