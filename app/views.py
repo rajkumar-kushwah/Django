e4edb0b2381e38e900881e24b5c8d6f56c44fe00
Sonu Lodhi <sp95759201@gmail.com>
2025-01-15 17:42:04 +0530

    add update

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import posts
 
@@ -15,3 +15,14 @@ def home(req):
         post.save()
         return HttpResponse("data added successfully")
     return render(req, 'app.html', {"posts": post})
+
+def update(req, id):
+    post = posts.objects.all()
+    updatePost = get_object_or_404(posts, id = id)
+    if req.method == 'POST':
+        updatePost.image = req.FILES.get('image')
+        updatePost.title = req.POST.get('title')
+        updatePost.des = req.POST.get('des')
+        updatePost.save()
+        return HttpResponse("update post")
+    return render(req, 'app.html' , {"posts": post, "update": updatePost})
